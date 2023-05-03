1b7fadafc7e1195383b4e92b3ae00dc28a2787f3
jflores <repositorydevcode@gmail.com>
2023-05-02 18:02:53 -0700

    çambios en el nombre del proyecto

--- ios/medicarsTests/medicarsTests.m
+++ ios/medicarsTests/medicarsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface medicartAppTests : XCTestCase
+@interface medicarsTests : XCTestCase
 
 @end
 
-@implementation medicartAppTests
+@implementation medicarsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
