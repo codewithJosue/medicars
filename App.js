568a52bd83bbe9f3add6da25911cfb8ee398aeb8
codewithJosue <repositorydevcode@gmail.com>
2023-06-06 22:09:35 -0600

    configuración de splash screen Android e icono de app

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import { Provider as PaperProvider } from "react-native-paper";
 import RootNavigations from "./src/navigations/RootNavigations";
+import { useEffect } from "react";
+import SplashScreen from "react-native-splash-screen";
 
 function App() {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  
   return (
     <PaperProvider>
       <RootNavigations />
